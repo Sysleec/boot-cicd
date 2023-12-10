bb1c65c9ce5d7996665ed96175600b1634cace49
sysleec <sysleec@gmail.com>
2023-12-10 20:57:49 +0300

    Update README

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("secrets.env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
